e2363a221f91b35bb2027b8704a46f65005307bf
MXMR-7 <maxime-maurin@hotmail.fr>
2017-01-04 20:38:36 +0100

    Add files via upload

---
+++ planManager.h
@@ -0,0 +1,31 @@
+#ifndef PLANMANAGER
+#define PLANMANAGER
+
+
+#include <iostream>
+#include <stdlib.h> 
+#include <stdio.h>
+#include <string.h>
+#include <plan.h>
+
+using namespace std;
+
+class PlanManager {
+protected:
+	
+public:
+	PlanManager();
+	// Plan(int identifier, int ver, GenericInstruction newInstruction);
+	void executePlan();
+	void getStatus();
+};
+
+
+
+
+
+
+
+
+
+#endif
