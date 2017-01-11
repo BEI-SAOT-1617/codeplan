c5694a36727a5494da4311e5aba31417f19e2c59
MXMR-7 <maxime-maurin@hotmail.fr>
2017-01-11 14:39:54 +0100

    Update planManager.cpp

--- planManager.cpp
+++ planManager.cpp
@@ -1,4 +1,15 @@
 #include "planManager.h"
+#include <plan.h>
+using namespace std;
 
+void PlanManager::executePlan(Plan P){
+ P.executePlan()
+}
 
+void PlanManager::getStatus(){
+  
+}
 
+Plan PlanManager::generationPlan(string path){
+  
+}
