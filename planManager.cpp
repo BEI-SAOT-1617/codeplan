badc152b7e74c58dd424805f35c2e99fb245d988
MXMR-7 <maxime-maurin@hotmail.fr>
2017-01-24 16:48:55 +0100

    Add files via upload

--- planManager.cpp
+++ planManager.cpp
@@ -18,7 +18,7 @@ PlanManager::PlanManager(){
 	this->nPlan = 0;
 	this->ptInstruction = 0;
 	this->indexPlan = 0;
-	this->bannedInstructions[]={false};
+	//this->bannedInstructions[]={false};
 }
 /* Merci Felix!! :) */
 
@@ -44,16 +44,16 @@ if (nPlan > 0)
 		struct tm * now = localtime(&t);
 
 		int group = currentInst->getIndex();
-		if (group == true) jumpInstruction = true;
-		
+		if ((this->bannedInstructions[group]) == true) jumpInstruction = true;
+
 		if (jumpInstruction == false) {
 			if ((now->tm_hour == currentInst->getHour()) & (now->tm_min == currentInst->getMin()) & (now->tm_sec == currentInst->getSec())) {
-			
+
 				if (currentInst->getType() == 'p') {
 
 					int exposure = currentInst->getExposure();
 					string photoName = currentInst->getPhotoName();
-					
+
 					exposure = 500;
 					cout << endl << "New photo! Smile!" << endl;
 					//myCameraController->photoShoot(photoName, exposure);
@@ -70,14 +70,14 @@ if (nPlan > 0)
 				}
 
 				ptInstruction++;
-				if (ptInstruction > P.getnInstructions()){ 
+				if (ptInstruction > P.getnInstructions()){
 				ptInstruction = 0;
 				indexPlan++;
-				indexPlan=indexPlan%5;
+				indexPlan=indexPlan%2;
 				nPlan--;
 				}
-				
-				
+
+
 				instructionCatched = false;
 			}
 		}
@@ -88,37 +88,54 @@ if (nPlan > 0)
 }
 
 Plan PlanManager::generatePlan(const char* filepath){
-	
+
 	unsigned int version;
 	unsigned int num_plan;
 	bool existnewPlan = true;
 
 	string s = filepath;
-	version = s[4]-'0';
-	num_plan = s[6]-'0';
-	
-	Plan myPlan(version, num_plan);
+	version = s[6]-'0';
+	num_plan = s[4]-'0';
+
+	Plan myPlan(num_plan, version);
 	myPlan.loadPlan(filepath);
-				
+
 	//Plans[nPlan].printPlan();
 
-	//cout << "version"<< version << endl;
-	//cout << "num_plan"<< num_plan<< endl;
+	cout << "version "<< version << endl;
+	cout << "num_plan "<< num_plan<< endl;
 
 	for (int i = 0 ; i < nPlan; i++){
-		if (Plans[(i+indexPlan)%5].getID()==num_plan){
-				if (Plans[(i+indexPlan)%5].getVersion() < version){
-					Plans[(i+indexPlan)%5]= myPlan;
+		cout << "nplan " << Plans[(i+indexPlan)%2].getID()<< endl;
+		if (Plans[(i+indexPlan)%2].getID()==num_plan){
+				cout << "hola " << Plans[(i+indexPlan)%2].getVersion()<<endl;
+				if (Plans[(i+indexPlan)%2].getVersion() < version){
+					Plans[(i+indexPlan)%2] = myPlan;
+					existnewPlan = false;
+					cout << " New version of an existing plan "<< endl;
 				}
-		existnewPlan=false;
 		}
 	}
+
 	if (existnewPlan){
-			Plans[(nPlan+indexPlan)%5]= myPlan;
+			Plans[(nPlan+indexPlan)%2]= myPlan;
 			nPlan++;
 	}
 
 }
+
+
+void PlanManager::printPlan(int indexPlan){
+     if (indexPlan >=nPlan){
+            cout << "Plan " << indexPlan << "does not exist"<< endl;
+        return;
+     }
+	this->Plans[indexPlan].printPlan();
+	return;
+
+}
+
+
 
 	void PlanManager::pushBan(int index){
 		this->bannedInstructions[index]= true;
