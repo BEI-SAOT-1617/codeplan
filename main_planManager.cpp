5a87386c45837b9ce22d107bd03d1f7536b934f6
Hellayne <helene.phouratsamay@gmail.com>
2017-01-25 11:47:12 +0100

    Update main_planManager.cpp

--- main_planManager.cpp
+++ main_planManager.cpp
@@ -2,6 +2,10 @@
 #include <string>
 #include <fstream>
 #include <time.h>
+#include <unistd.h>
+#include <signal.h>
+
+#define LOW 0
 
 #include "cameraController.h"	
 #include "attitudeController.h"
@@ -9,6 +13,12 @@
 
 #include "statusManager.h"
 #include "watchdog.h"
+#include "GPIO.h"
+
+// Pour lancer :
+// g++ main_planManager.cpp attitudeController.cpp cameraController.cpp planManager.cpp plan.cpp genericInstruction.cpp statusManager.cpp GPIO.cpp Watchdog.cpp -o exec 
+
+// Pour changer la date = sudo date --set "xx:xx:xx"
 
 
 using namespace std;
@@ -19,22 +29,33 @@ char mode; // 'F' pour follower et 'L' pour leader
 // En pratique, le mode devra aussi être envoyé au ComGroundManager
 
 
+void changeMode() {
+	// passage en mode Leader : le mode doit être envoyé au ComGroundManager
+	mode='L';
+}
+
 void before() {
 // Vérification watchdog, recouvrement si besoin
 
 	if (mode=='L') {
 	watchdog.set(); 		// I'm alive!!!!!
+
+	cout << "Leader : I'm alive" << endl;
+
 	}
 
 
 	if (mode=='F') { 
 		sleep(1);
 		int state;
-		state = watchdog.read(); // test watchdog
+	cout << "Follower : Test watchdog" << endl;
+		state = watchdog.readw(); // test watchdog
+	cout << "State " << state << endl;
 
 		// Recovery
-		if(c==0) {
-			mode='L'; // changement de mode
+		if(state==0) {
+	cout << "Follower : changement de mode" << endl;
+			changeMode(); // changement de mode
 		}
 	}
 
@@ -43,18 +64,28 @@ void before() {
 
 void proceed() {
 // Fonctionnement normal
-	planManager.executePlan(); // à modifier
+	cout << "Fonctionnement" << endl;
+	planManager.executePlan();
 
 
 }
 
+sig_t bye() {
+	cout << "Mort........" << endl;
+	GPIOWrite(3,LOW);
+	exit(0);
+}
+
 void after() {
-// .... do nothing
+	// .... do nothing
 }
 
 int main(int argc, char** argv) {
 	
 	mode = 'F'; // par défaut, follower
+	planManager.generatePlan("plan1_1.txt");
+
+	signal(SIGINT, (sig_t)bye);
 
 	while(1) {
 		before();
@@ -64,4 +95,3 @@ int main(int argc, char** argv) {
 
 	return 0;
 }
-
